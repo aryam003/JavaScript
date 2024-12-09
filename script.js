42386da8d4b8c5dca6618b706c89c0859458b4aa
aryam003 <aryamarya537@gmail.com>
2024-12-09 13:30:00 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,76 @@
+// alert("javascript")
+
+// a="Script"
+// alert(a)
+// console.log("welcome",a);
+
+// var a="java"
+// var a=20
+
+// let a="java"
+// a=0
+
+// const a="Js"
+
+// ----------------------------------
+
+// number
+// let a=90 
+// let a=2.56 
+// let a=(1,2,3) 
+
+// object
+// let a=[1,2,3]
+// [1,2,3]
+
+// let a={name:'A',age:23} 
+// {name:'A',age:23} 
+
+// number Nan:not a number
+// let a
+// a+=10
+// console.log(typeof a);
+// console.log(a);
+
+// -------------------------------------
+
+// console.log(1+2);
+//  (  -,+,/,%,*,**, ++,-- )
+//                   ------
+// let a=1
+// a++
+// a+1=2
+
+// let a=1
+// a--
+// a-1=0
+
+// ( +=,-=,*=,/=,%=,**=)
+// let a=10
+// a*=10
+// 100
+
+// ( ==,!=,<,>,<=,>=, === , !== )
+//                   ------------
+// let a=1
+// let b="1"
+// console.log(a==b);
+// t
+// console.log(a===b);
+// f
+
+// let a=1
+// let b="1"
+// console.log(a!=b);
+// f
+// console.log(a!==b);
+// t
+
+let a=1
+let b="1"
+// console.log(a!=b && a==10);
+// f  (&& = and)
+console.log(a!=b || a==1);
+// t  (|| = or)
+
+
